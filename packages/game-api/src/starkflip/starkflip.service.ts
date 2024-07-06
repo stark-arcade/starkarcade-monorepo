0213052061d9d3a48061716aa97f9fd79fc69a64
kafierence <vannam34760@gmail.com>
2024-07-06 11:00:08 +0700

    [update] flip rank

--- packages/game-api/src/starkflip/starkflip.service.ts
+++ packages/game-api/src/starkflip/starkflip.service.ts
@@ -1,6 +1,11 @@
-import { ChainDocument, Chains } from '@app/shared/models/schemas';
+import {
+  ChainDocument,
+  Chains,
+  StarkFlip,
+  StarkFlipDocument,
+} from '@app/shared/models/schemas';
 import { StarkFlipEvents, StarkFlipStatus } from '@app/shared/types';
-import { ABIS, StarkFlipEnum } from '@app/web3/types';
+import { ABIS } from '@app/web3/types';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -22,6 +27,8 @@ export class StarkFlipService {
     @InjectModel(Chains.name)
     private readonly chainModel: Model<ChainDocument>,
     private readonly settleWorker: SettleWorker,
+    @InjectModel(StarkFlip.name)
+    private readonly starkflipModel: Model<StarkFlipDocument>,
   ) {}
 
   private sendGameResult(socket: Socket) {
@@ -55,5 +62,67 @@ export class StarkFlipService {
     } catch (error) {
       throw new WsException(error.message);
     }
+  }
+
+  // Rest API
+  async handleGetLeaderboard() {
+    const results = await this.starkflipModel
+      .aggregate([
+        {
+          $group: {
+            _id: '$player',
+            totalFlipped: { $sum: 1 },
+            winAmount: {
+              $sum: {
+                $cond: ['$isWon', { $toDouble: '$stakedAmount' }, 0],
+              },
+            },
+            lostAmount: {
+              $sum: {
+                $cond: ['$isWon', 0, { $toDouble: '$stakedAmount' }],
+              },
+            },
+            wins: { $sum: { $cond: ['$isWon', 1, 0] } },
+            losses: { $sum: { $cond: ['$isWon', 0, 1] } },
+          },
+        },
+        {
+          $sort: { winAmount: -1 },
+        },
+        {
+          $limit: 10,
+        },
+        {
+          $project: {
+            player: '$_id',
+            totalFlipped: 1,
+            winAmount: 1,
+            lostAmount: 1,
+            wins: 1,
+            losses: 1,
+            total: { $sum: ['$winAmount', '$lostAmount'] },
+          },
+        },
+        {
+          $sort: { winAmount: -1 },
+        },
+        {
+          $project: {
+            _id: 0,
+            player: 1,
+            totalFlipped: 1,
+            winAmount: 1,
+            wins: 1,
+            losses: 1,
+            rank: { $literal: 0 },
+          },
+        },
+      ])
+      .exec();
+
+    return results.map((item, index) => ({
+      ...item,
+      rank: index + 1,
+    }));
   }
 }
